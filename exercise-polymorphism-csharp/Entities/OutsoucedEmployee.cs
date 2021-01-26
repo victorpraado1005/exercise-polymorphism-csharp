97a8b8fc6fc5a22f902c1f8520276402c3f27a38
Victor Prado <prado.victor1005@gmail.com>
2021-01-26 15:11:27 -0300

    Exercicio finalizado

---
+++ exercise-polymorphism-csharp/Entities/OutsoucedEmployee.cs
@@ -0,0 +1,22 @@
+﻿namespace exercise_polymorphism_csharp.Entities
+{
+    class OutsoucedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsoucedEmployee()
+        {
+        }
+
+        public OutsoucedEmployee(string name, int hours, double valuePerHour, double additionalCharge) : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge;
+
+        }
+    }
+}
